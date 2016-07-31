6e4d74e6ac0e330465615e52f4fa7bfd03ff8e5e
Philipp Schuster <phip1611@gmail.com>
2016-08-01 00:28:49 +0200

    first version

---
+++ src/de/phip1611/kmp_algorithm/KMP.java
@@ -0,0 +1,35 @@
+package de.phip1611.kmp_algorithm;
+
+/**
+ * Von: Philipp Schuster (@phip1611)
+ * Stand: 2016-08-01 August
+ *
+ * Beschreibung:
+ *  Berechnet die Verschiebetabelle für ein Pattern nach dem KMP-Algorithmus
+ *  beziehungsweise nach der Vorschrift, wie sie von Professor Vogler an der TU Dresden gelehrt wird.
+ */
+public class KMP {
+    public static int[] analyze(String pattern) {
+        int[] verschiebetabelle = new int[pattern.length()];
+        // Fortschritt zeigt bis wohin die Tabelle schon ausgefüllt ist
+        int zeiger = 0, schrittweite = 1, fortschritt = 0;
+        verschiebetabelle[0] = -1;
+        while (zeiger+schrittweite < pattern.length()) {
+            if (pattern.charAt(zeiger) == pattern.charAt(zeiger+schrittweite)) {
+                if (fortschritt+1 == zeiger+schrittweite) {
+                    verschiebetabelle[zeiger+schrittweite] = verschiebetabelle[zeiger];
+                    fortschritt++;
+                }
+                zeiger++;
+            } else {
+                if (fortschritt+1 == zeiger+schrittweite) {
+                    verschiebetabelle[zeiger+schrittweite] = zeiger;
+                    fortschritt++;
+                }
+                zeiger = 0;
+                schrittweite++;
+            }
+        }
+        return verschiebetabelle;
+    }
+}
---
+++ src/de/phip1611/kmp_algorithm/Main.java
@@ -0,0 +1,18 @@
+package de.phip1611.kmp_algorithm;
+
+import java.util.Arrays;
+
+public class Main {
+    
+
+    public static void main(String[] args) {
+        String pattern1 = "aaaaa"; // -1 -1 -1 -1 -1
+        System.out.println(Arrays.toString(KMP.analyze(pattern1)));
+        String pattern2 = "ababba"; // -1 0 -1 0 2 -1
+        System.out.println(Arrays.toString(KMP.analyze(pattern2)));
+        String pattern3 = "ababaabb"; // -1 0 -1 0 -1 3 0 2
+        System.out.println(Arrays.toString(KMP.analyze(pattern3)));
+        String pattern4 = "abbabbaa"; // -1 0 0 -1 0 0 -1 4
+        System.out.println(Arrays.toString(KMP.analyze(pattern4)));
+    }
+}
