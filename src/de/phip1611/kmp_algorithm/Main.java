79b8d16bc5e545b2b7a29e96da75fd083ce55e07
Philipp Schuster <phip1611@gmail.com>
2016-08-01 01:37:26 +0200

    Terminalmodus, GUI

--- src/de/phip1611/kmp_algorithm/Main.java
+++ src/de/phip1611/kmp_algorithm/Main.java
@@ -1,8 +1,46 @@
 package de.phip1611.kmp_algorithm;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI();
+        boolean  terminalMode = true;
+        if (args.length > 0) {
+            if (args[0].equals("-gui")) {
+                terminalMode = false;
+                GUI gui = new GUI();
+            }
+        }
+        if (terminalMode) {
+            System.out.println("Patterns mit dem KMP-Algorithmus berechnen");
+            System.out.println("Von Philipp Schuster (@phip1611)");
+            System.out.println("(so wie es an der TU Dresden gelehrt wird)");
+            System.out.println("===");
+            System.out.println("Das Programm kann jederzeit mit \":exit\" beendet werden");
+            System.out.println("Es kann jederzeit mit \":gui\" eine GUI gestartet werden");
+            System.out.println("Wird das Programm mit dem Terminal-Befehl \"-gui\" aufgerufen, startet direkt die GUI");
+            System.out.println("===");
+            Scanner scanner = new Scanner(System.in);
+            String input;
+            boolean startGui = false;
+            while (true) {
+                System.out.println("Bitte die Zeichenkette/das Pattern eingeben:");
+                System.out.print("Pattern: ");
+                input = scanner.nextLine();
+                if (input.isEmpty()) continue;
+                if (input.equals(":exit")) System.exit(0);
+                if (input.equals(":gui")) {
+                    startGui = true;
+                    break;
+                }
+                System.out.println("Verschiebetabelle:");
+                System.out.println(Arrays.toString(KMP.analyze(input)));
+            }
+            if (startGui) {
+                new GUI();
+            }
+        }
 
         /*String pattern1 = "aaaaa"; // -1 -1 -1 -1 -1
         System.out.println(Arrays.toString(KMP.analyze(pattern1)));
